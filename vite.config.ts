811dda15a85649634a3f0be6dbc63ce0751839ea
Zuruuh <ziadi.mail.pro@gmail.com>
2023-07-27 14:26:39 +0200

    Configure ladle for static build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import { defineConfig } from 'vite';
 import packageJson from './package.json' assert { type: 'json' };
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
-import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig((env) => ({
   plugins: [
     react({ jsxRuntime: env.command === 'build' ? 'classic' : 'automatic' }),
     dts({ insertTypesEntry: true }),
-    tsConfigPaths(),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src', 'index.ts'),
-      name: '@zuuruh/react-date-picker',
+      name: '@zuruuh/react-date-picker',
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
