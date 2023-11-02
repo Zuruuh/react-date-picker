bc67f302f1abd0a3c0a50f89f784dca2267fedbd
Zuruuh <ziadi.mail.pro@gmail.com>
2023-11-03 00:50:38 +0100

    Merge happydom.ts and bootstrap.tsx

--- bootstrap.tsx
+++ bootstrap.tsx
@@ -1,4 +1,6 @@
-import { afterEach } from 'bun:test';
+import { afterEach, beforeAll } from 'bun:test';
 import { cleanup } from '@testing-library/react';
+import { GlobalRegistrator } from '@happy-dom/global-registrator';
 
+beforeAll(GlobalRegistrator.register);
 afterEach(cleanup);
