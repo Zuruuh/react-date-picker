79e94b9b623ed7cbc5f4e0de9cab7934e1861721
Zuruuh <ziadi.mail.pro@gmail.com>
2023-11-03 00:51:05 +0100

    Start working on tests

--- src/components/Calendar.spec.tsx
+++ src/components/Calendar.spec.tsx
@@ -2,21 +2,55 @@ import { test, expect } from 'bun:test';
 import { generateWeeksBasedOnOverlap } from './Calendar';
 import { type Dayjs } from 'dayjs';
 import { type DatePickerCalendarOverlap } from '../context/DatePickerContext';
-import { WeekNumbers } from '../types/WeekNumber';
 import { day } from '../tests/utils';
 
-type Output = [Dayjs, DatePickerCalendarOverlap, WeekNumbers];
+type Output = [Dayjs, DatePickerCalendarOverlap];
 
 const expectedOutputs: Output[] = [
-  [
-    day({ year: 2023, month: 10, day: 23 }),
-    'overlap',
-    [[39], [40], [41], [42], [43], [44]],
-  ] satisfies Output,
+  [day({ year: 2023, month: 1, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 2, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 3, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 4, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 5, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 6, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 7, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 8, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 9, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 10, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 11, day: 1 }), 'overlap'],
+  [day({ year: 2023, month: 12, day: 1 }), 'overlap'],
+
+  [day({ year: 2023, month: 1, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 2, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 3, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 4, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 5, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 6, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 7, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 8, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 9, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 10, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 11, day: 1 }), 'no-overlap'],
+  [day({ year: 2023, month: 12, day: 1 }), 'no-overlap'],
+
+  [day({ year: 2023, month: 1, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 2, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 3, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 4, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 5, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 6, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 7, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 8, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 9, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 10, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 11, day: 1 }), 'no-overlap-with-offset'],
+  [day({ year: 2023, month: 12, day: 1 }), 'no-overlap-with-offset'],
 ];
 
 test('generateWeeksBasedOnOverlap', () => {
-  expectedOutputs.forEach(([date, type, output]) => {
-    expect(Array.from(generateWeeksBasedOnOverlap(date, type))).toEqual(output);
+  expectedOutputs.forEach(([date, type]) => {
+    expect(
+      Array.from(generateWeeksBasedOnOverlap(date, type)),
+    ).toMatchSnapshot();
   });
 });
